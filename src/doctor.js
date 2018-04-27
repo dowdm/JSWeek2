cc84f61da741ffd0b5fc0246a455e0716aedec86
Matt Dowdney <theofficialmatt@gmail.com>
2018-04-27 10:11:45 -0700

    add Doctor class and contructor

---
+++ src/doctor.js
@@ -0,0 +1,12 @@
+class Doctor {
+  constructor(firstName, lastName, street, city, phoneNumber, webAddress, newPatients){
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.street = street;
+    this.city = city;
+    this.state = "OR";
+    this.phoneNumber = phoneNumber;
+    this.webAddress = webAddress;
+    this.newPatients = newPatients;
+  }
+}
