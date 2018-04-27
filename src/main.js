cf8799755380fa0f88903b8ae10775908e87e43f
Matt Dowdney <theofficialmatt@gmail.com>
2018-04-27 08:54:23 -0700

    adds js for accepting input and appending parsed results

--- src/main.js
+++ src/main.js
@@ -1,9 +1,23 @@
-// import { function } from './class';
-// import 'bootstrap';
-// import 'bootstrap/dist/css/bootstrap.min.css';
-// import './styles.css';
-// import $ from 'jquery';
-//
-// $(document).ready(function() {
-//
-// });
+import { Api } from './api';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles.css';
+import $ from 'jquery';
+
+$(document).ready(function() {
+  $("#doctorFinder").submit(function(event){
+    event.preventDefault();
+    let api = new Api();
+    let promise = api.makeCall();
+    promise.then(function(response) {
+      let body = JSON.parse(response);
+      $("#output").append(body);
+
+    },
+     function(error) {
+      $("#output").text(`There was an error processing your request: ${error.message}`);
+
+
+    });
+  });
+});
