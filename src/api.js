dfcc5217cc5640a7cf894dd0cdb977094717cea8
Matt Dowdney <theofficialmatt@gmail.com>
2018-04-27 08:51:29 -0700

    adds generic api call with apikey placeholder

---
+++ src/api.js
@@ -0,0 +1,19 @@
+class Api {
+  makeCall(){
+    let promise = new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?location=37.773,-122.413,100&skip=2&limit=10&user_key=${process.env.exports.apiKey}`;
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+      }
+      request.open("GET", url, true);
+      request.send();
+    });
+    return promise;
+  }
+}
+export { Api };
